7cbb47f8dd09b81ae0b96a78ac73f5117dc90d23
Edson Rodrigo <tec.er@outlook.com>
2016-06-19 22:16:26 -0300

    teste

--- src/main/java/br/aprendendoweb/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/br/aprendendoweb/persistencia/jdbc/ConexaoFactory.java
@@ -14,7 +14,7 @@ public class ConexaoFactory {
 			//Usei o Class.forname para que possamos manipular os dados do banco via java web
 			//ou seja estou forçando....
 			Class.forName("org.postgresql.Driver");
-			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/projetoweb", "postgres", "tel24M20");
+			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/projetoweb", "postgres", "1234");
 		} catch (SQLException | ClassNotFoundException e) {
 			e.printStackTrace();
 			throw new RuntimeException();
